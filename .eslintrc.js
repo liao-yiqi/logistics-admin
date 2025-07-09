22b7728b57e9d4cc46bf8f73f5b6a0cff12a2883
liaoyiqi <liaoyq@saichen.cn>
2025-07-09 11:19:57 +0800

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // 整合 prettier
+  ],
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off',
+  },
+}
